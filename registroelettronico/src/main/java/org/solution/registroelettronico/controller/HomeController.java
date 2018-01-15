f01d5e7e0c5950e39462edaaf36772d90d82c070
landrogiuseppe <landrogiuseppe@SPD-LANDRO.office.corp.sia.it>
2018-01-15 18:31:16 +0100

    entity amanager factory

--- registroelettronico/src/main/java/org/solution/registroelettronico/controller/HomeController.java
+++ registroelettronico/src/main/java/org/solution/registroelettronico/controller/HomeController.java
@@ -1,11 +1,15 @@
 package org.solution.registroelettronico.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	  private LocalContainerEntityManagerFactoryBean entityManagerFactoryBean;
 	
 	@RequestMapping(value ="/greeting")
 	public String sayHello (Model model) {
