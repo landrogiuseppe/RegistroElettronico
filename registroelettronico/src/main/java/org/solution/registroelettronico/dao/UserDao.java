8b779e54ce7096ed0475023b3a481d1538acb674
LandroGiuseppe <LandroGiuseppe@SPD-LANDRO.office.corp.sia.it>
2018-01-19 17:57:49 +0100

    jndi

--- registroelettronico/src/main/java/org/solution/registroelettronico/dao/UserDao.java
+++ registroelettronico/src/main/java/org/solution/registroelettronico/dao/UserDao.java
@@ -1,6 +1,32 @@
 package org.solution.registroelettronico.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
 public class UserDao {
+	
+	
+	  @Autowired
+	  private SessionFactory sessionFactory;
+	  
+	  
+	  @Transactional
+	  public Object ricercaDaId(long id) {
+	    
+		  Object found = null;
+	    try{
+	    	Session sessione = sessionFactory.getCurrentSession();
+	    	found= (Object) sessione.get(Object.class, id);
+	    }catch(Exception e){
+	    	System.out.println(e);
+	    }
+	   
+		return found;
+	  }
 
 
 }
